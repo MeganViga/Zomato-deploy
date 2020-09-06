e02accfa1c36496c26de4cad952c588347f70128
MeganViga <viganesh.s.m@gmail.com>
2020-09-06 15:13:57 +0530

    first

---
+++ build/precache-manifest.017360a1f62b73028767eec0742ee874.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea5d04070334a6229643cf3c1c8d0f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e67f050181cdbef5ec3",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "043740fe56cc39eab314",
+    "url": "/static/css/main.b928107b.chunk.css"
+  },
+  {
+    "revision": "3e67f050181cdbef5ec3",
+    "url": "/static/js/2.fc952720.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fc952720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043740fe56cc39eab314",
+    "url": "/static/js/main.3566439e.chunk.js"
+  },
+  {
+    "revision": "0529a98c11a206755e39",
+    "url": "/static/js/runtime-main.99490353.js"
+  }
+]);
